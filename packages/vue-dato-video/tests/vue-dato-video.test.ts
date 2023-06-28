206a8fc0f92a7eaf489577a8d60e70c11471ad17
Selwyn <talk@selwyn.cc>
2023-06-28 16:18:31 +0200

    Fix warnings

--- packages/vue-dato-video/tests/vue-dato-video.test.ts
+++ packages/vue-dato-video/tests/vue-dato-video.test.ts
@@ -3,12 +3,12 @@ import { shallowMount } from '@vue/test-utils'
 import VueDatoVideo from '../src/vue-dato-video.vue'
 
 test('Renders', () => {
-  // @ts-expect-error
   const wrapper = shallowMount(VueDatoVideo, {
     props: {
       loop: false,
       autoplay: false,
       mute: false,
+      playIconAlt: 'Play video',
       video: {
         url: 'https://www.youtube.com/watch?v=AoIq7P6DJKU',
         title: 'A Love Letter to Winter',
--- packages/vue-lazy-load/tests/vue-lazy-load.test.ts
+++ packages/vue-lazy-load/tests/vue-lazy-load.test.ts
@@ -3,16 +3,12 @@ import { shallowMount } from '@vue/test-utils'
 import VueLazyLoad from '../src/vue-lazy-load.vue'
 
 test('Renders', () => {
-  // No idea how to fix this, PR's are welcome.
-  // @ts-expect-error
   const wrapper = shallowMount(VueLazyLoad)
   expect(wrapper.exists()).toBe(true)
 })
 
 test('Shows placeholder', () => {
   const placeholderHtml = '<div class="placeholder"></div>'
-  // No idea how to fix this, PR's are welcome.
-  // @ts-expect-error
   const wrapper = shallowMount(VueLazyLoad, {
     slots: {
       placeholder: placeholderHtml,
